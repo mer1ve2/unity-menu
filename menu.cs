8adf177018ab34336c1132c9d5727a11cbe953b9
Merve Yiğit <37611702+mer1ve2@users.noreply.github.com>
2019-02-18 23:46:33 +0300

    Add files via upload

---
+++ menu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
